fa347998c82fde75854e772f8afd1fd37a46360b
marchingband <youthclubrecords@gmail.com>
2021-12-12 19:37:46 -0800

    v1.0.9

--- src/wvr_0.3.h
+++ src/wvr_0.3.h
@@ -1,9 +1,7 @@
 #ifndef WVR_0_3_H
 #define WVR_0_3_H
 
-// #include "WVR.h"
-
-#define VERSION_CODE "1.0.7"
+#define VERSION_CODE "1.0.9"
 
 void wvr_init(bool useFTDI, bool useUsbMidi, bool checkRecoveryModePin);
 
